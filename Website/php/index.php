bf2a3724662d6339cf12ac03864f43e12f232152
krish-jain <krish-jain@users.noreply.github.com>
2020-07-20 11:47:50 -0700

    another

--- Website/php/index.php
+++ Website/php/index.php
@@ -62,15 +62,15 @@ if(isset($_POST["submit"]))
   require 'class/class.phpmailer.php';
   $mail = new PHPMailer;
   $mail->IsSMTP();        //Sets Mailer to send message using SMTP
-  $mail->Host = 'smtpout.secureserver.net';  //Sets the SMTP hosts
-  $mail->Port = '80';        //Sets the default SMTP server port
-  $mail->SMTPAuth = true;       //Sets SMTP authentication. Utilizes the Username and Password variables
-  $mail->Username = 'xxxxxxxxxx';     //Sets SMTP username
-  $mail->Password = 'xxxxxxxxxx';     //Sets SMTP password
+  $mail->Host = '';  //Sets the SMTP hosts
+  $mail->Port = '25';        //Sets the default SMTP server port
+  $mail->SMTPAuth = false;       //Sets SMTP authentication. Utilizes the Username and Password variables
+  $mail->Username = 'info@scienceinfinity.org';     //Sets SMTP username
+  $mail->Password = 'Blue.Water1';     //Sets SMTP password
   $mail->SMTPSecure = '';       //Sets connection prefix. Options are "", "ssl" or "tls"
   $mail->From = $_POST["email"];     //Sets the From email address for the message
   $mail->FromName = $_POST["name"];    //Sets the From name of the message
-  $mail->AddAddress('info@find2rent.com', 'Name');//Adds a "To" address
+  $mail->AddAddress('info@scienceinfinity.org', 'Name');//Adds a "To" address
   $mail->AddCC($_POST["email"], $_POST["name"]); //Adds a "Cc" address
   $mail->WordWrap = 50;       //Sets word wrapping on the body of the message to a given number of characters
   $mail->IsHTML(true);       //Sets message type to HTML
